978217eed8aef956da367757ee61cfc2c7dc967e
Navneet Singh Gill <gillsupremacy@navneets-mbp.home>
2023-05-03 13:29:04 -0400

    setup initial project

---
+++ mobile-coding-challenge/mobile-coding-challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile-coding-challenge
+//
+//  Created by Navneet Singh Gill on 2023-05-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
