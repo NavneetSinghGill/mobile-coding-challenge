c1f2599f62240c58b44cf8cd7ea61935ea2b0e74
Navneet Singh Gill <gillsupremacy@navneets-mbp.home>
2023-05-03 16:21:00 -0400

    add podcasts tableview cell

---
+++ mobile-coding-challenge/mobile-coding-challenge/TableviewCells/PodcastsTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  PodcastsTableViewCell.swift
+//  mobile-coding-challenge
+//
+//  Created by Navneet Singh Gill on 2023-05-03.
+//
+
+import UIKit
+
+struct PodcastsTableViewCellConstants {
+    let cornerRadiusOfThumbnail: CGFloat = 5
+}
+
+class PodcastsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var publisherNameLabel: UILabel!
+    @IBOutlet weak var favouriteLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        thumbnailImageView.layer.cornerRadius = PodcastsTableViewCellConstants().cornerRadiusOfThumbnail
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
